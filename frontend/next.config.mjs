94b687f20f7316672d595b1566ed2a7420d6161a
DIPESH <dipesh@DIPESHs-MacBook-Air.local>
2025-08-05 02:22:23 +0530

    Temporarily disable Sentry in next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,40 +1,61 @@
-import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+};
 
-export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+export default nextConfig;
 
-// Suppresses source map uploading logs during build
-silent: true,
-org: "javascript-mastery",
-project: "javascript-nextjs",
-}, {
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+// Sentry temporarily disabled
+// import { withSentryConfig } from '@sentry/nextjs';
+// export default withSentryConfig(nextConfig, {
+//   silent: true,
+//   org: "javascript-mastery",
+//   project: "javascript-nextjs",
+// }, {
+//   widenClientFileUpload: true,
+//   transpileClientSDK: true,
+//   hideSourceMaps: true,
+//   disableLogger: true,
+//   automaticVercelMonitors: true,
+// });
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+// import {withSentryConfig} from '@sentry/nextjs';
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-// Transpiles SDK to be compatible with IE11 (increases bundle size)
-transpileClientSDK: true,
+// export default withSentryConfig(nextConfig, {
+// // For all available options, see:
+// // https://github.com/getsentry/sentry-webpack-plugin#options
 
-// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-// tunnelRoute: "/monitoring",
+// // Suppresses source map uploading logs during build
+// silent: true,
+// org: "javascript-mastery",
+// project: "javascript-nextjs",
+// }, {
+// // For all available options, see:
+// // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
+// // Upload a larger set of source maps for prettier stack traces (increases build time)
+// widenClientFileUpload: true,
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+// // Transpiles SDK to be compatible with IE11 (increases bundle size)
+// transpileClientSDK: true,
 
-// Enables automatic instrumentation of Vercel Cron Monitors.
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+// // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+// // This can increase your server load as well as your hosting bill.
+// // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+// // side errors will fail.
+// // tunnelRoute: "/monitoring",
+
+// // Hides source maps from generated client bundles
+// hideSourceMaps: true,
+
+// // Automatically tree-shake Sentry logger statements to reduce bundle size
+// disableLogger: true,
+
+// // Enables automatic instrumentation of Vercel Cron Monitors.
+// // See the following for more information:
+// // https://docs.sentry.io/product/crons/
+// // https://vercel.com/docs/cron-jobs
+// automaticVercelMonitors: true,
+// });
